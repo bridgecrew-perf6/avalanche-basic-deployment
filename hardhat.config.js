80bce6853d2f86778ca4c47fc8d6f6915e6d1d8b
monikkacha <monikkacha@gmail.com>
2022-04-24 11:22:58 +0530

    demo for the launching contract on avalanche

---
+++ hardhat.config.js
@@ -0,0 +1,41 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+// Replace these private key with your Avalanche account private keys
+// Be aware of NEVER adding private keys to GIT
+const AVALANCHE_TEST_PRIVATE_KEY = "ACCOUNT_PRIVATE_KEY_HERE";
+const AVALANCHE_MAIN_PRIVATE_KEY = "ACCOUNT_PRIVATE_KEY_HERE";
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    avalancheTest: {
+      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      gasPrice: 225000000000,
+      chainId: 43113,
+      accounts: [`0x${AVALANCHE_TEST_PRIVATE_KEY}`]
+    },
+    avalancheMain: {
+      url: 'https://api.avax.network/ext/bc/C/rpc',
+      gasPrice: 225000000000,
+      chainId: 43114,
+      accounts: [`0x${AVALANCHE_MAIN_PRIVATE_KEY}`]
+    }
+  }
+};
